991c7fc1cca6dcba3b2ba302cf0b9238cea366be
Aditya Kumar <aditya32193213@gmail.com>
2026-01-19 00:42:02 +0530

    Added sentry error tracking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+
+// ✅ Initialize Sentry as early as possible (CRA way)
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +15,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
